a9c66213e58b55ac99381b01913249c3150f9972
Kabeaman <96170087+Kabeaman@users.noreply.github.com>
2023-08-15 17:24:48 -0300

    Delete dadoOtro.py

--- dadoOtro.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def dado():
-    return random.randint(1,6)
-
-lanzar_dado = dado()
-lanzar_dado2 = dado()
-lanzar_dado3= dado()
-print("Los dados han caido en " + str(lanzar_dado)+ ", " + str(lanzar_dado2) + " y " + str(lanzar_dado3))
