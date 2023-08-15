7d32d277ea16c6127372b7930a3647dc0a9e2e90
Kabeaman <96170087+Kabeaman@users.noreply.github.com>
2023-08-15 17:01:26 -0300

    Delete Regalos directory

--- Regalos/hyreayun.py
+++
@@ -1,8 +0,0 @@
-from art import *
-tprint("Feliz",font="block",chr_ignore=True)
-print()
-print()
-tprint("navidad",font="block",chr_ignore=True)
-print()
-print()
-tprint("Cami",font="block",chr_ignore=True)
